be8e469b13a852b96567001dc918e43033763574
Nurettin Şen <nurie487@gmail.com>
2022-06-12 14:41:47 +0300

    Add cron job

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ require("express-async-errors")
 const express = require("express")
 const app = express()
 const helmet = require("helmet")
+const mongoose = require("mongoose")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const RouterArrFns = require("./routes/index")
-const req = require("express/lib/request")
-
+const Models = require("./mongodb/messagesModel")
+const exampleDataInformation = require("./mongodb/data/index")
 const router = express.Router();
 const client = require("./redis/index")
+const cron = require("node-cron")
+
+let redisConnected = false
+let mongoConnected = false
+
+const  RedisMessagesCacheWorker = require("./workers/RedisUsersCacheWorker")
 
 RouterArrFns.forEach((routerFn,index)=>{
     routerFn(router)
@@ -35,10 +42,44 @@ client.on("error", (err)=>{
 
  //baglantıyı gerçeklestırelım
 client.connect().then(()=>{
+    
 console.log("client connected")
+  redisConnected = true
+
 }).catch(err =>{
     console.log(err)
 })
+
+
+
+//mongoo connect
+
+mongoose.connect("mongodb://localhost:27017/redis").then(async()=>{
+console.log("mongodb connected")
+mongoConnected = true
+// await exampleDataInformation()
+}).catch(err=>{
+    console.log(err)
+})
+
+cron.schedule('* * * * * ',()=>{
+    if(redisConnected && mongoConnected){
+        RedisMessagesCacheWorker().then((res)=>{
+            if(res){
+                console.log("users are synchorized with redis cache system")
+            }
+        }).catch(err=>{
+            console.log(err)
+        })
+    
+    }else{
+        console.log("you can run the worker because of connectıon not established")
+    }
+   
+
+})
+
+
 
 const server = app.listen(7000,()=>{
     console.log("bu port dınlenıyor: " + 7000)
