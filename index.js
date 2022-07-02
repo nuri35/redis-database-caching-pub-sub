50bd38447d81bb72b5a78e0e8503e83c4396c83d
Nurettin Şen <nurie487@gmail.com>
2022-07-02 11:06:29 +0300

    Update

--- index.js
+++ index.js
@@ -1,66 +1,66 @@
-require("express-async-errors")
-const express = require("express")
-const app = express()
-const helmet = require("helmet")
-const mongoose = require("mongoose")
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const RouterArrFns = require("./routes/index")
-const Models = require("./mongodb/messagesModel")
-const exampleDataInformation = require("./mongodb/data/index")
+require("express-async-errors");
+const express = require("express");
+const app = express();
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const RouterArrFns = require("./routes/index");
+const Models = require("./mongodb/messagesModel");
+const exampleDataInformation = require("./mongodb/data/index");
 const router = express.Router();
-const client = require("./redis/index")
-const cron = require("node-cron")
+const client = require("./redis/index");
+const cron = require("node-cron");
 
-let redisConnected = false
-let mongoConnected = false
+let redisConnected = false;
+let mongoConnected = false;
 
-const  RedisMessagesCacheWorker = require("./workers/RedisUsersCacheWorker")
+const RedisMessagesCacheWorker = require("./workers/RedisUsersCacheWorker");
 
-RouterArrFns.forEach((routerFn,index)=>{
-    routerFn(router)
-})
+RouterArrFns.forEach((routerFn, index) => {
+  routerFn(router);
+});
 
-app.use(cors())
-app.use(helmet())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(bodyParser.json({limit:"50mb",extended:true})) 
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
 
-app.use('/api',router)
+app.use("/api", router);
 
+//state durum baglantının durumuyla ılgılı
+client.on("connect", () => {
+  console.log("redis client connected");
+});
 
-//state durum baglantının durumuyla ılgılı 
-client.on("connect", ()=>{
-console.log("redis client connected")
-})
+client.on("error", (err) => {
+  console.log("redis client error", err);
+});
 
-
-client.on("error", (err)=>{
-    console.log("redis client error",err)
-    })
-
- //baglantıyı gerçeklestırelım
-client.connect().then(()=>{
-    
-console.log("client connected")
-  redisConnected = true
-
-}).catch(err =>{
-    console.log(err)
-})
-
-
+//baglantıyı gerçeklestırelım
+client
+  .connect()
+  .then(() => {
+    console.log("client connected");
+    redisConnected = true;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //mongoo connect
 
-mongoose.connect("mongodb://localhost:27017/redis").then(async()=>{
-console.log("mongodb connected")
-mongoConnected = true
-// await exampleDataInformation()
-}).catch(err=>{
-    console.log(err)
-})
+mongoose
+  .connect("mongodb://localhost:27017/redis")
+  .then(async () => {
+    console.log("mongodb connected");
+    mongoConnected = true;
+    // await exampleDataInformation()
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // cron.schedule('* * * * * ',()=>{
 //     if(redisConnected && mongoConnected){
@@ -71,19 +71,15 @@ mongoConnected = true
 //         }).catch(err=>{
 //             console.log(err)
 //         })
-    
+
 //     }else{
 //         console.log("you can run the worker because of connectıon not established")
 //     }
-   
 
 // })
 
+const server = app.listen(7000, () => {
+  console.log("bu port dınlenıyor: " + 7000);
+});
 
-
-const server = app.listen(7000,()=>{
-    console.log("bu port dınlenıyor: " + 7000)
-})
-
-
-module.exports = server
+module.exports = server;
--- routes/cacheRouter.js
+++ routes/cacheRouter.js
@@ -1,112 +1,94 @@
-const client = require("./../redis/index")
-const Message = require("./../mongodb/messagesModel")
+const client = require("./../redis/index");
+const Message = require("./../mongodb/messagesModel");
 
 module.exports = (router) => {
-    
-    router.get("/cached/users",async(req,res)=>{
-   
-      const obj =   await client.hGetAll("user943230509")
-  
-        res.send(obj)
+  router.get("/cached/users", async (req, res) => {
+    const obj = await client.hGetAll("user943230509");
 
-    })
+    res.send(obj);
+  });
 
-    router.get("/cached/users-cached",async(req,res)=>{
-        
-        try{
-            const cacheKey ="users/first50";
-            let users = await client.get(cacheKey)
+  router.get("/cached/users-cached", async (req, res) => {
+    try {
+      const cacheKey = "users/first50";
+      let users = await client.get(cacheKey);
 
-            if(!users){ 
-               
-                users =  await client.hGetAll("user943230509") 
-                await client.set(cacheKey,JSON.stringify(users))
+      if (!users) {
+        users = await client.hGetAll("user943230509");
+        await client.set(cacheKey, JSON.stringify(users));
+      } else {
+        users = JSON.parse(users);
+      }
 
-            }else{
-                users = JSON.parse(users)
-            }
+      res.send(users);
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
-            res.send(users)
+  /* Absolute timing  */
+  router.get("/cached/users-cached-absolute", async (req, res) => {
+    const cacheKey = "users/expire/first50";
+    let users = await client.get(cacheKey);
 
-        }catch(err){
-            console.log(err)
-        }
-         
-      })
+    if (!users) {
+      users = await client.hGetAll("user943230509");
+      await client.set(cacheKey, JSON.stringify(users), {
+        EX: 60,
+        NX: true,
+        KEEPTTL: true,
+      });
+    } else {
+      users = JSON.parse(users);
+    }
+    res.send({ message: "cached users with absolute timing", users: users });
+  });
 
-/* Absolute timing  */
-      router.get("/cached/users-cached-absolute",async(req,res)=>{
-   const cacheKey = "users/expire/first50"
-   let users = await client.get(cacheKey)
+  /* Sliding timing  */
 
-   if(!users){
-    users =  await client.hGetAll("user943230509") 
-    await client.set(cacheKey,JSON.stringify(users),{
-        EX:60,
-        NX:true,
-        KEEPTTL:true
-    })
-   }else{
-    users = JSON.parse(users)
-   }
-   res.send({message:"cached users with absolute timing",users:users})
+  router.get("/cached/users-cached-sliding", async (req, res) => {
+    try {
+      const cacheKey = "users/expire/first50";
+      let users = await client.get(cacheKey);
 
-      })
+      if (!users) {
+        users = await client.hGetAll("user943230509");
+        await client.set(cacheKey, JSON.stringify(users), {
+          EX: 60,
+          NX: true,
+          KEEPTTL: true,
+        });
+      } else {
+        const GETEX_OLD = await client.ttl(cacheKey);
+        console.log("cache would have been expired in ", GETEX_OLD, " seconds");
+        await client.expire(cacheKey, 60);
+        const GETEX_NEW = await client.ttl(cacheKey);
+        console.log("cache will have been expired in ", GETEX_NEW, "seconds");
 
-      /* Sliding timing  */
+        users = JSON.parse(users);
+      }
 
-      router.get("/cached/users-cached-sliding",async(req,res)=>{
-          try{
+      res.send({ message: "cached users with silding timing", users: users });
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
-            const cacheKey = "users/expire/first50"
-            let users = await client.get(cacheKey)
-         
-            if(!users){
-             users =  await client.hGetAll("user943230509") 
-             await client.set(cacheKey,JSON.stringify(users),{
-                 EX:60,
-                 NX:true,
-                 KEEPTTL:true
-             })
-            }else{
-                const GETEX_OLD = await client.ttl(cacheKey)
-                console.log("cache would have been expired in ", GETEX_OLD, " seconds");
-                await client.expire(cacheKey,60)
-                const GETEX_NEW = await client.ttl(cacheKey)
-                console.log("cache will have been expired in ", GETEX_NEW, "seconds");
+  router.get("/cached/messages", async (req, res) => {
+    const messages = await Message.find({}).limit(50).lean();
+    res.json({ msg: "NON CACHED MESSAGE", messages });
+  });
 
-                users = JSON.parse(users)
-            }
-            
-            res.send({message:"cached users with silding timing",users:users})
+  router.get("/cached/messages-cached", async (req, res) => {
+    const cacheKey = "messages/1";
 
-          }catch(err){
-              console.log(err)
-          }
-      
-     
-           })
+    let messages = await client.json.get(cacheKey);
 
+    if (!messages) {
+      messages = await Message.find({}).limit(50).lean();
+      await client.json.set(cacheKey, "$", messages);
+    }
 
-           router.get("/cached/messages",async(req,res)=>{
-   
-            const messages =  await Message.find({}).limit(50).lean()
-            res.json({msg:"NON CACHED MESSAGE",messages})
-          })
-
-
-          router.get("/cached/messages-cached",async(req,res)=>{
-              const cacheKey = "messages/1"
-               
-              let messages = await client.json.get(cacheKey) 
-
-              if(!messages){
-                messages =   await Message.find({}).limit(50).lean()
-                await client.json.set(cacheKey,'$',messages)
-              }
-             
-            res.json({msg:" CACHED MESSAGE",messages})
-          })
-
-
-}
+    res.json({ msg: " CACHED MESSAGE", messages });
+  });
+};
